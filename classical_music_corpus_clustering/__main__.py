0e0cc608777c84c436558e95027faa462a01074e
Guilherme Marthe <gui.marthe@ime.usp.br>
2024-10-01 17:00:41 -0300

    refactor for corpora handling

--- classical_music_corpus_clustering/__main__.py
+++ classical_music_corpus_clustering/__main__.py
@@ -2,37 +2,68 @@ import fsspec
 from pathlib import Path
 import pandas as pd
 import ms3
+from dataclasses import dataclass
+from typing import Optional
 
 
-def get_df_by_position_with_name(position, dfs):
+def get_df_by_position_with_name(position, dfs) -> pd.DataFrame:
     key = list(dfs.keys())[position]
     return dfs[key]["expanded"][0][1].assign(piece=key)
 
 
-def download_corpus_harmony(repo: str, destination: Path):
+def download_corpus_harmony(repo: str, destination: Path) -> None:
     fs = fsspec.filesystem("github", org="dcmlab", repo=repo)
     fs.get(fs.ls("harmonies/"), destination.as_posix())
     fs.get("metadata.tsv", (destination / "metadata.tsv").as_posix())
 
 
-if __name__ == "__main__":
-
-    destination = Path(".") / "corpus"
+def make_destination(repo: str) -> Path:
+    destination = Path(".") / "corpus" / repo
     destination.mkdir(exist_ok=True, parents=True)
+    return destination
 
-    download_corpus_harmony(repo="mozart_piano_sonatas", destination=destination)
-    corpus = ms3.Corpus("corpus/")
 
-    corpus.parse_tsv()
+@dataclass
+class CorpusMD():
+    repo: str
+    destination: Path
+    corpus_ms3 = Optional[ms3.corpus.Corpus]
+
+    @property
+    def composer(self) -> str:
+        return self.repo.split('_')[0].title()
+
+    def get_dataframes(self):
+        self.corpus_ms3.parse_tsv()
+        return self.corpus_ms3.get_dataframes()
+
+
+def download_corpora(corpora: list[CorpusMD]):
+    for corpus in corpora:
+        download_corpus_harmony(corpus.repo, corpus.destination)
+
+
+def extract_dataframes(corpus: CorpusMD):
+    corpus.corpus_ms3 = ms3.Corpus(corpus.destination.as_posix())
     dfs = corpus.get_dataframes()
-    adf = get_df_by_position_with_name(0, dfs)
-
     cdf = pd.concat(
         [
-            get_df_by_position_with_name(i, dfs).assign(composer="Mozart")
+            get_df_by_position_with_name(i, dfs).assign(composer=corpus.composer)
             for i in range(len(dfs))
         ]
     )
+    return cdf
+
+
+if __name__ == "__main__":
+
+    repos = ['mozart_piano_sonatas', 'beethoven_piano_sonatas']
+
+    corpora = [CorpusMD(repo, make_destination(repo)) for repo in repos]
+
+    download_corpora(corpora)
+
+    cdf = pd.concat([extract_dataframes(corpus) for corpus in corpora])
 
     tone_frequency_by_piece = (
         cdf.loc[:, "piece composer chord_tones".split()]
